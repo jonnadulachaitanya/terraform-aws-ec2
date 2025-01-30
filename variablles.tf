a991318991f59d93ab487a20e2d8f634915fd043
chaitanyajonnadula <chaitanyajonnadula98@gmail.com>
2025-01-30 22:52:07 +0530

    file committed

--- variablles.tf
+++ variablles.tf
@@ -4,8 +4,14 @@ variable "ami_id" {
 }
 
 #It is optional, it will take default as t3.micro. User can override
+#You can restrict the other instance_type by using below condition. It will prevent the mistakes.
 variable "instance_type" {
     default = "t3.micro"
+    type = string
+    validation {
+        condition = contains(["t3.micro","t3.medium","t3.small"], var.instance_type)
+        error_message = "instance_type can be one of t3.micro,t3.medium,t3.small"
+    }
 }
 
 #It is mandatory, user should supply
